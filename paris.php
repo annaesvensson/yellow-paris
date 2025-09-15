c0f48c7c8f1b77127c2ad01b310ab345b22ed808
markseu <mark2011@mayberg.se>
2025-09-15 20:33:10 +0200

    Renamed Markdown block elements

--- paris.php
+++ paris.php
@@ -2,7 +2,7 @@
 // Paris extension, https://github.com/annaesvensson/yellow-paris
 
 class YellowParis {
-    const VERSION = "0.9.2";
+    const VERSION = "0.9.3";
     public $yellow;         // access to API
     
     // Handle initialisation
